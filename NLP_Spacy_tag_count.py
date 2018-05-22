30b44fe75dad652a6763686832eb5c1b02d43e28
Leevi Rantala <leevirantala@gmail.com>
2018-05-22 11:23:11 +0300

    Updated gitignore

--- NLP_Spacy_tag_count.py
+++ NLP_Spacy_tag_count.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 import itertools
 import spacy
 import pandas as pd
+pd.options.mode.chained_assignment = None
 import logging
 import csv
 import regex as re
@@ -30,6 +31,7 @@ def save_file(file, name):
     logging.debug("Finished writing: " + name)
 
 def create_chunks(list, size):
+    logging.debug("Creating a chunk")
     return (list[pos:pos + size] for pos in range(0, len(list), size))
 
 def main():
@@ -38,7 +40,10 @@ def main():
     logging.debug("Entering main")
 
     #Load csv as pandas
-    df = open_file("10k-test_comments.csv")
+    df = open_file("Comments0_210k_comments.csv")
+    #df = open_file("Comments200_400k_comments.csv")
+    #df = open_file("Comments400_rest_comments.csv")
+    #df = open_file("10k-test_comments.csv")
     df.fillna('', inplace=True)
 
 
@@ -59,13 +64,15 @@ def main():
     # pos = []
 # Iterating through the blocks of the big list
     count = 0
-    for x in create_chunks(df, 1000):
-        tokens = []
-        lemma = []
-        pos = []
+    for x in create_chunks(df, 10000):
+        # tokens = []
+        # lemma = []
+        # pos = []
+        # tag = []
+        dep = []
+        combined = []
         count += 1
-        texts = x["text"]
-
+        logging.debug("Starting iteration")
     # i = 0
         for doc in nlp.pipe(x['text'].astype('unicode').values):
             # for doc in nlp.pipe(df['text'].astype('unicode').values):
@@ -74,21 +81,29 @@ def main():
             #         print(i)
             if doc.is_parsed:
                 #
-                tokens.append([n.text for n in doc])
-                lemma.append([n.lemma_ for n in doc])
-                pos.append([n.pos_ for n in doc])
+                # tokens.append([n.text for n in doc])
+                # lemma.append([n.lemma_ for n in doc])
+                # pos.append([n.pos_ for n in doc])
+                # tag.append([n.tag_ for n in doc])
+                # dep.append([n.dep_ for n in doc])
+
+                # This was used originally. Gives only simple POS tags.
+                # combined.append([(n.lemma_, n.pos_) for n in doc])
+                combined.append([(n.lemma_, n.tag_, n.dep_) for n in doc])
             else:
                 # We want to make sure that the lists of parsed results have the
                 # same number of entries of the original Dataframe, so add some blanks in case the parse fails
-
-                tokens.append(None)
-                lemma.append(None)
-                pos.append(None)
+                combined.append(None)
+                # tokens.append(None)
+                # lemma.append(None)
+                # pos.append(None)
+                # tag.append(None)
         #
         print("Setting tokens to df")
-        x['tokens'] = tokens
-        x['lemmas'] = lemma
-        x['pos'] = pos
+        x["lemma_tag_dep"] = combined
+        # x['tokens'] = tokens
+        # x['lemmas'] = lemma
+        # x['pos'] = pos
 
         # df['tokens'] = tokens
         # df['lemmas'] = lemma
@@ -98,7 +113,10 @@ def main():
         save_file(x, str(count))
 
     # save_file(df)
+    logging.debug("All processed and saved. Resetting dataframe.")
+    df = []
 
 
 if __name__ == '__main__':
     main()
+    logging.debug("Program ran successfully.")
